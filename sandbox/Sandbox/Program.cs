1c9fc2fea73ad56763192a44f361f6854342dd22
Luisa <luisaandreina2701@gmail.com>
2026-09-17 09:32:35 -0600

    Things that I like

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello this is Luisa!");
+        Console.WriteLine("I'm from Ecuador and I like cevide de pinchagua");
     }
 }
